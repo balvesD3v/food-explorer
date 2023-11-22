490a464645dad862436de7a7eb8ea71ef8fb37f6
balvesD3v <lpbalves7filho@gmail.com>
2023-11-22 17:38:25 -0300

    feat: somes fixes

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload");
 
 const app = express();
 const port = 3000;
 app.use(express.json());
-
 app.use(cors());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
 app.use(routes);
 
 app.use((error, request, response, next) => {
